e85bb51b9ec88f68f4ba3e2f3db3b4ffca313364
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-08 15:47:39 -0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">Vue with TailwindCSS</h1>
+
+    <button @click="increment" class="px-4 py-2 bg-green-300 my-36">
+      Count is: {{ state.count }} Double is: {{ state.double }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+    });
+
+    function increment() {
+      state.count++;
+    }
+
+    return {
+      state,
+      increment,
+    };
+  },
+};
+</script>
