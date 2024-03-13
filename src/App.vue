82b1cc5fd9d4ff3df295dbcdf05385fb215d238f
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-13 15:41:20 -0700

    fix viewport scaling and improve ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container mx-auto relative h-screen">
+  <div class="h-screen relative">
     <nav
-      class="text-sm px-3 h-8 flex justify-between items-center bg-black text-white"
+      class="sticky top-0 text-sm px-3 h-8 flex justify-between items-center bg-black text-white"
     >
       <a href="/"
         ><h2 class="text-md font-medium text-pink-400">
@@ -10,7 +10,9 @@
       >
 
       <div class="flex justify-between items-center gap-5">
-        <a href="https://js.langchain.com/docs/expression_language/cookbook"><p>LangChain</p></a>
+        <a href="https://js.langchain.com/docs/expression_language/cookbook"
+          ><p>LangChain</p></a
+        >
         <a href="https://vuejs.org/guide/introduction.html"><p>Vue</p></a>
         <a href="https://tailwindcss.com/"><p>TailwindCSS</p></a>
       </div>
@@ -35,10 +37,10 @@
       </div>
     </section>
 
-    <section id="chat" class="mt-8">
+    <section id="chat" class="pt-4 px-4">
       <div
         id="chat-card"
-        class="flex flex-col max-w-sm mx-auto rounded-md overflow-hidden h-[450px]"
+        class="flex flex-col max-w-sm md:max-w-lg mx-auto rounded-md overflow-hidden h-[450px]"
       >
         <div class="flex justify-center items-center bg-black py-3 text-white">
           <p class="text-xs font-semibold">
@@ -50,9 +52,11 @@
         </div>
         <div
           id="response"
-          class="bg-gray-200 p-3 text-white h-64 max-h-64 flex flex-col"
+          class="bg-gray-200 p-3 text-white h-44 md:h-64 flex flex-col"
         >
-          <div class="text-xs mb-1 text-gray-800">AI Response</div>
+          <div class="border-l-0 border border-solid border-b-gray-400 text-xs text-gray-800 flex justify-left items-center rounded-tl-sm rounded-tr-sm overflow-hidden">
+            <p class="bg-gray-300 py-2 px-3">AI Response</p>
+          </div>
           <div
             class="p-3 bg-gray-300 h-full rounded-sm text-sm overflow-x-auto overflow-y-auto text-gray-800"
           >
@@ -72,9 +76,10 @@
             </template>
           </div>
         </div>
-        <div id="prompt" class="bg-gray-200 text-sm px-3 pb-2 flex flex-col">
-          <div class="text-xs mb-1 text-gray-800">Human Prompt</div>
-
+        <div id="prompt" class="bg-gray-200 text-sm p-3 flex flex-col">
+          <div class="border-l-0 border border-solid border-b-gray-400 text-xs text-gray-800 flex justify-left items-center rounded-tl-sm rounded-tr-sm overflow-hidden">
+            <p class="bg-white py-1 px-3">Human Prompt</p>
+          </div>
           <textarea
             v-model="input"
             class="text-gray-800 w-full overflow-y-auto p-3 rounded-sm"
@@ -95,7 +100,7 @@
 
     <footer
       id="footer"
-      class="absolute text-sm px-3 h-8 flex justify-between items-center bg-black text-white w-full bottom-0"
+      class="sticky text-sm px-3 h-8 flex justify-between items-center bg-black text-white w-full bottom-0"
     >
       <h2 class="text-md font-medium text-pink-400">
         chatter<span class="text-white font-bold">Box</span>
